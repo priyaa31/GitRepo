bb17fd2987ea2035e9c79c9b55171dc1eed302e8
Owner <Owner@Owner-PC>
2017-12-11 11:01:39 +0530

    first commit

---
+++ src/gitDemo/Testing.java
@@ -0,0 +1,17 @@
+package gitDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testing 
+{
+	public static void main(String[] args) 
+	{
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver","D:/Selenium/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("http://www.rue21.com");
+		
+	}
+
+}
